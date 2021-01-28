bdeeee558776825cdab48a387bdd46e8acca1a5f
Daksh C Reddy <70056557+dakshreddy@users.noreply.github.com>
2021-01-28 16:06:47 +0530

    Add files via upload

---
+++ Bedtime-stories-master/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import {TabNavigator} from "./components/AppTabNavigator"
+import {AppDrawerNavigator} from './components/AppDrawerNavigator';
+
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <AppContainer />
+      
+    );
+  }
+}
+
+
+
+const switchNavigator = createSwitchNavigator({
+  Welcome: {screen: WelcomeScreen},
+  Drawer: {screen : AppDrawerNavigator},
+  BottomTab: {screen: TabNavigator},
+})
+
+const AppContainer =  createAppContainer(switchNavigator);
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
